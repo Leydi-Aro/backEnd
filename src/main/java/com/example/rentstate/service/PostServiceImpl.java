1184b40146309df1d789309ca0da1b30e3d84322
Angie_Naye_Ruiz <89089445+Angie-RC@users.noreply.github.com>
2023-11-03 23:43:06 -0500

    create postserviceImpl

---
+++ src/main/java/com/example/rentstate/service/PostServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.rentstate.service;
+
+import com.example.rentstate.domain.service.PostService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostServiceImpl implements PostService {
+    
+}
